45e8833425c1be1279039af3984ed6f9972a8efd
K <kihyuk.sung@outlook.kr>
2021-10-28 18:23:58 +0900

    Refactor : 적용한 규칙으로 컴파일이 되도록 수정
    기본으로 생성된 파일들을 규칙에 맞도록 수정하였습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
