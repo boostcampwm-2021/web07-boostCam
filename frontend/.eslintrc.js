262f4ff2d6d0f90151bdc4291a48e6706a813e5f
korung3195 <vs970517@naver.com>
2021-10-28 19:18:20 +0900

    Refactor : 세팅 파일들 리팩토링
    eslint 설정 파일, prettier 설정 파일들도 prettier 설정에 맞게 리팩토링하였습니다.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'airbnb-typescript',
     'prettier',
     'plugin:react/recommended', // eslint-plugin-react에서 추천하는 리액트 린팅 설정
-    'plugin:@typescript-eslint/recommended',  // @typescript-eslint/recommended의 추천 룰 사용
+    'plugin:@typescript-eslint/recommended', // @typescript-eslint/recommended의 추천 룰 사용
     'plugin:prettier/recommended',
   ],
   plugins: ['prettier', 'react', '@typescript-eslint', 'react-hooks'], // 해당 플러그인을 사용할것이라고 등록
@@ -16,17 +16,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,  // jsx 활성화
+      jsx: true, // jsx 활성화
     },
     ecmaVersion: 2021,
     sourceType: 'module', // import 사용
     project: './tsconfig.json',
   },
   rules: {
-    "react/react-in-jsx-scope": "off",
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
   },
 };
