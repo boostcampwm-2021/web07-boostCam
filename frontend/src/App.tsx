28e42bf6ffade71ba5b1f41a5f01ae36bb75df48
K <kihyuk.sung@outlook.kr>
2021-11-01 15:54:39 +0900

    Feat : frontend에서 socket.io message 이벤트 발생
    테스트하기 위한 용도로 message 이벤트를 발생시키고, receive 이벤트를
    받습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
+import { io } from 'socket.io-client';
 import Screen from './components/Screen';
 
 function App(): JSX.Element {
+  const socket = io();
+  socket.on('connect', () => {
+    console.log('connected!');
+  });
+
+  socket.emit('message');
+
+  socket.on('receive', (data) => {
+    console.log(data);
+  });
+
   return (
     <div className="App">
       <Screen />
