82a0ea8b27806ad4d79e665266945183bf800e6d
pjs7160@gmail.com <49611158+Suppplier@users.noreply.github.com>
2021-11-02 18:11:48 +0900

    Refactor : ChattingTab의 방 join 코드 변경
    별도로 방에 참가하는 이벤트를 발생시키는 input을 삭제하고 지정된 방에 바로 참가하도록 코드를 수정하였습니다.
    추후 Cam이 정상적으로 동작하게 하기 위해서는 socket이 Cam 페이지에 진입할 때 특정 방에 join하도록 코드를 수정해야 합니다.

--- frontend/src/components/Cam/ChattingTab.tsx
+++ frontend/src/components/Cam/ChattingTab.tsx
@@ -16,7 +16,7 @@ const Container = styled.div<{ isActive: boolean }>`
 
 function ChattingTab(props: ChattingTabProps): JSX.Element {
   const [chatLogs, setChatLogs] = useState<string[]>([]);
-  const [room, setRoom] = useState<string | null>(null);
+  const [room, setRoom] = useState<string | null>('init');
   const { socket, isChattingTabActive } = props;
 
   const sendMessage = (e: React.KeyboardEvent<HTMLInputElement>): void => {
@@ -32,17 +32,6 @@ function ChattingTab(props: ChattingTabProps): JSX.Element {
     }
   };
 
-  const enterRoom = (e: React.KeyboardEvent<HTMLInputElement>): void => {
-    const { key, currentTarget } = e;
-    if (key === 'Enter') {
-      const { value } = currentTarget;
-      if (!value) alert('올바른 방 이름을 입력해주세요!');
-      else setRoom(value);
-      socket.emit('setRoom', value);
-      currentTarget.value = '';
-    }
-  };
-
   useEffect(() => {
     socket.on('receiveMessage', (msg: string) => {
       setChatLogs((logs) => [...logs, msg]);
@@ -55,8 +44,6 @@ function ChattingTab(props: ChattingTabProps): JSX.Element {
 
   return (
     <Container isActive={isChattingTabActive}>
-      <div>currentRoom : {room}</div>
-      <input type="text" placeholder="참가할 방을 선택하세요" onKeyDown={enterRoom} />
       <div>{currentChatLogs}</div>
       <input type="text" placeholder="내용을 입력하세요." onKeyDown={sendMessage} />
     </Container>
