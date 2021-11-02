96e6864d5cc401dd4ff504f2484de3653fded613
pjs7160@gmail.com <49611158+Suppplier@users.noreply.github.com>
2021-11-02 15:20:56 +0900

    Feat : styled-component 모듈 추가

---
+++ frontend/src/components/Cam.tsx
@@ -0,0 +1,13 @@
+import React, { MutableRefObject, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+function Cam(): JSX.Element {
+  return <Container>Cam</Container>;
+}
+
+export default Cam;
