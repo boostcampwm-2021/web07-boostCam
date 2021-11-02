f05513235942fe75c9db6cf4636eabd55cb536ec
pjs7160@gmail.com <49611158+Suppplier@users.noreply.github.com>
2021-11-02 15:39:23 +0900

    Feat : GlobalStyle 적용
    GlobalStyle에 대해서 margin과 padding을 0으로 초기화

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+
 import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
