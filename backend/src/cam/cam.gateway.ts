a4e5a023f9198c7b2066a52a235474bd232a5159
korung3195 <vs970517@naver.com>
2021-11-08 21:58:28 +0900

    Feat : 백엔드 cam 관련 map 구현
    - 기존에 socket만을 통해 같은 방 사용자들의 status를 동기화시키는 것에 한계가 존재하기 때문에, 백엔드에 Map을 만들어서 관리하기로 결정하였습니다.
    - CamService라는 service을 만들어서 api를 구현하고, 그것을 CamGateway에 주입하였습니다.
    - status type을 백엔드에도 파일로 만들었습니다.

--- backend/src/cam/cam.gateway.ts
+++ backend/src/cam/cam.gateway.ts
@@ -1,21 +1,37 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
+import Status from 'src/types/cam';
+import { CamService } from './cam.service';
+
 @WebSocketGateway()
 export class CamGateway {
+  constructor(private camService: CamService) {
+    this.camService.createRoom('1');
+  }
   @SubscribeMessage('joinRoom')
   handleMessage(
     client: Socket,
-    payload: { roomId: string; userId: string },
+    payload: { roomId: string; userId: string; status: Status },
   ): void {
-    const { roomId, userId } = payload;
+    const { roomId, userId, status } = payload;
     client.join(roomId);
+    this.camService.joinRoom(roomId, userId, status);
     client.to(roomId).emit('userConnected', { userId });
+
     client.on('disconnect', () => {
       client.to(roomId).emit('userDisconnected', { userId });
+      this.camService.exitRoom(roomId, userId);
     });
-    client.on('userChangeStatus', ({ userId, status }) => {
-      client.to(roomId).emit('userChangeStatus', { userId, status });
+
+    client.on('updateUserStatus', ({ status }) => {
+      this.camService.updateStatus(roomId, userId, status);
+      client.to(roomId).emit('userStatus', { userId, status });
+    });
+
+    client.on('getUserStatus', ({ userId }) => {
+      const status = this.camService.getStatus(roomId, userId);
+      client.emit('userStatus', { userId, status });
     });
   }
 }
--- backend/src/cam/cam.module.ts
+++ backend/src/cam/cam.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CamGateway } from './cam.gateway';
+import { CamService } from './cam.service';
 
 @Module({
-  providers: [CamGateway],
+  providers: [CamGateway, CamService],
 })
 export class CamModule {}
