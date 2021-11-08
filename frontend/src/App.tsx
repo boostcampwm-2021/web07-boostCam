ffa01958007d13d14bd3d0a5bbea8fb67e7d4630
pjs7160@gmail.com <49611158+Suppplier@users.noreply.github.com>
2021-11-08 16:08:08 +0900

    Feat : react-router-dom 모듈 설치
    라우팅을 위한 react-router-dom과 @types/react-router-dom을 설치하였습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { RecoilRoot } from 'recoil';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Cam from './components/Cam/Cam';
 
