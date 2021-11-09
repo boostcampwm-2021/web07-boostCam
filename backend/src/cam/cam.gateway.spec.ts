4c027bb4bed5c58452715adc836460ce5088709d
korung3195 <vs970517@naver.com>
2021-11-09 14:45:53 +0900

    Fix : 테스트 코드 추가
    cam service 관련 테스트 로직을 추가하였습니다.

--- backend/src/cam/cam.gateway.spec.ts
+++ backend/src/cam/cam.gateway.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CamGateway } from './cam.gateway';
+import { CamService } from './cam.service';
 
 describe('CamGateway', () => {
   let gateway: CamGateway;
+  let service: CamService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CamGateway],
+      providers: [CamGateway, CamService],
     }).compile();
 
     gateway = module.get<CamGateway>(CamGateway);
+    service = module.get<CamService>(CamService);
   });
 
   it('should be defined', () => {
     expect(gateway).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
