0b53d370c7664b07b101b46dd78bd4ed91a71ff0
K <kihyuk.sung@outlook.kr>
2021-11-11 20:14:02 +0900

    Feat : SharedScreenStore
    SharedScreenReceiver와 Sender와 관련한 로직을 Context로 저장하는 컴포넌트
    입니다. 기존의 버튼 handle 기능도 이곳으로 옮기고, sharedScreen 상태를
    가집니다.

---
+++ frontend/src/components/Cam/SharedScreenStore.tsx
@@ -0,0 +1,72 @@
+import React, { createContext, useEffect, useRef, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+
+import SharedScreenReceiver from '../../hooks/SharedScreenReceiver';
+import SocketState from '../../atoms/socket';
+import SharedScreenSender from '../../hooks/SharedScreenSender';
+
+type SharedScreenStoreProps = {
+  children: React.ReactChild[] | React.ReactChild;
+};
+
+export const SharedScreenStoreContext = createContext<React.ComponentState>(null);
+
+function SharedScreenStore(props: SharedScreenStoreProps): JSX.Element {
+  const { children } = props;
+  const socket = useRecoilValue(SocketState);
+
+  const [sharedScreen, setSharedScreen] = useState<MediaStream | null>(null);
+  const [sharedFromMe, setSharedFromMe] = useState(false);
+
+  const sharedScreenReceiverRef = useRef<SharedScreenReceiver>();
+  const sharedScreenSenderRef = useRef<SharedScreenSender>();
+
+  const currentURL = new URL(window.location.href);
+  const roomId = currentURL.searchParams.get('roomid');
+
+  useEffect(() => {
+    if (!roomId) {
+      return;
+    }
+    sharedScreenReceiverRef.current = new SharedScreenReceiver(socket, roomId, setSharedScreen);
+  }, []);
+
+  const tryShareScreen = async () => {
+    if (!roomId) {
+      return;
+    }
+    try {
+      const stream = await navigator.mediaDevices.getDisplayMedia();
+      stream.getVideoTracks()[0].addEventListener('ended', () => {
+        setSharedScreen(null);
+        sharedScreenSenderRef.current?.stopSharingScreen();
+      });
+      setSharedScreen(stream);
+      setSharedFromMe(true);
+      sharedScreenSenderRef.current = new SharedScreenSender(socket, roomId);
+      sharedScreenSenderRef.current.prepareScreenShare(stream);
+    } catch (error) {
+      // do nothing
+    }
+  };
+
+  const handleScreenShareActive = (): void => {
+    if (!sharedScreen) {
+      tryShareScreen();
+    }
+
+    if (sharedScreen && sharedFromMe) {
+      setSharedScreen(null);
+      setSharedFromMe(false);
+      sharedScreenSenderRef.current?.stopSharingScreen();
+    }
+  };
+
+  return (
+    <SharedScreenStoreContext.Provider value={{ sharedScreen, setSharedScreen, handleScreenShareActive }}>
+      {children}
+    </SharedScreenStoreContext.Provider>
+  );
+}
+
+export default SharedScreenStore;
