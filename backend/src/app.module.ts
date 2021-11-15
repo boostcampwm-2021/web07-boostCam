6d222b4deab3e6f0275a009a77e75f24ffbbbc71
pjs7160@gmail.com <49611158+Suppplier@users.noreply.github.com>
2021-11-15 17:43:19 +0900

    Feat : @nestjs/config 모듈 추가, DB 연결
    - backend에 @nestjs/config 모듈을 추가하였습니다.
    - Mysql DB 와 Nestjs의 서버를 연결하였습니다.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import ormConfig from './config/ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CamModule } from './cam/cam.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CamModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(ormConfig()),
+    CamModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
