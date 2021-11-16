91de40aefc7429159b66dd7ff50987f6e80c08fd
K <kihyuk.sung@outlook.kr>
2021-11-16 20:29:41 +0900

    Feat : GitHub 로그인 시에 db에 유저가 존재하는 경우 반환
    그 외 존재하지 않는 경우는 NotFoundException이 발생하며, 그렇지 않으면
    BadRequestException이 발생합니다.

--- backend/src/login/login.service.ts
+++ backend/src/login/login.service.ts
@@ -1,5 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
+import { Repository } from 'typeorm';
+
+import { User } from '../user/user.entity';
 
 import githubConfig from '../config/github.config';
 
@@ -7,8 +17,44 @@ import githubConfig from '../config/github.config';
 export class LoginService {
   constructor(
     @Inject(githubConfig.KEY) private config: ConfigType<typeof githubConfig>,
+    @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
-  async githubLogin(code: string) {
-    console.log(this.config);
+  async githubLogin(code: string): Promise<User> {
+    try {
+      const accessTokenResponse = await axios.post(
+        'https://github.com/login/oauth/access_token',
+        {
+          client_id: this.config.clientID,
+          client_secret: this.config.clientSecret,
+          redirect_uri: this.config.callbackURL,
+          code,
+        },
+        {
+          headers: { Accept: 'application/json' },
+        },
+      );
+      const { access_token: accessToken } = accessTokenResponse.data;
+      const githubUserResponse = await axios.get(
+        'https://api.github.com/user',
+        {
+          headers: { Authorization: `token ${accessToken}` },
+        },
+      );
+
+      const { id: githubId, avator_url } = githubUserResponse.data;
+      const user = await this.userRepository.findOne({ where: { githubId } });
+
+      if (!user) {
+        throw new NotFoundException();
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+
+      throw new HttpException('bad request', 400);
+    }
   }
 }
