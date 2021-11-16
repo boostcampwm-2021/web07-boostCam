c75cb3dc7300b03c91038b96aaff678fb83471b3
pjs7160@gmail.com <49611158+Suppplier@users.noreply.github.com>
2021-11-16 18:49:47 +0900

    Feat : 서버의 이름이 들어갈 MainHeader를 작성

--- frontend/src/components/Main/MainHeader.tsx
+++ frontend/src/components/Main/MainHeader.tsx
@@ -12,13 +12,31 @@ const Container = styled.div`
   background-color: #321832;
 `;
 
+const HeaderBox = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const CurrentServerName = styled.span`
+  color: #dcd6d0;
+  font-size: 30px;
+
+  margin-left: 20px;
+`;
+
 function MainHeader(): JSX.Element {
   const socket = useRecoilValue(socketState);
   const navigate = useNavigate();
 
   useEffect(() => {}, []);
 
-  return <Container>Server Name</Container>;
+  return (
+    <Container>
+      <HeaderBox>
+        <CurrentServerName>Server Name</CurrentServerName>
+      </HeaderBox>
+    </Container>
+  );
 }
 
 export default MainHeader;
