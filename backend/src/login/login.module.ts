2f11f0969a30da732a94cc8bb8bdb212182e0c24
K <kihyuk.sung@outlook.kr>
2021-11-17 17:49:09 +0900

    Feat : login guard 추가
    세션의 존재 유무를 확인하여 접근 권한을 설정할 수 있습니다.

--- backend/src/login/login.module.ts
+++ backend/src/login/login.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 
 import { UserModule } from '../user/user.module';
 import { LoginController } from './login.controller';
+import { LoginGuard } from './login.guard';
 import { LoginService } from './login.service';
 
 @Module({
   imports: [ConfigModule, UserModule],
   controllers: [LoginController],
-  providers: [LoginService],
+  providers: [LoginService, LoginGuard],
+  exports: [LoginGuard],
 })
 export class LoginModule {}
