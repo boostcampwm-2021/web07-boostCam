c0a658547876d65c900fa3ce687295829b87a7dd
K <kihyuk.sung@outlook.kr>
2021-11-17 17:26:12 +0900

    Feat : 세션 사용 설정
    express-session을 이용하여 세션을 사용하도록 설정하였습니다.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressPeerServer } from 'peer';
+import * as session from 'express-session';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const server = app.getHttpServer();
   const peerServer = ExpressPeerServer(server);
+
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 
   app.use('/peerjs', peerServer);
   await app.listen(9000);
