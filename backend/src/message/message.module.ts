1c3ce5693e31aeafa19ba97028c8810b35dd0a28
K <kihyuk.sung@outlook.kr>
2021-11-22 18:56:19 +0900

    Feat : MessageModule에서 관련 객체들을 사용
    Service와 Controller, 그리고 typeorm 관련 entity를 추가합니다.

--- backend/src/message/message.module.ts
+++ backend/src/message/message.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Message } from './message.entity';
+import { MessageController } from './message.controller';
+import { MessageService } from './message.service';
+import { User } from '../user/user.entity';
+import { Channel } from '../channel/channel.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [TypeOrmModule.forFeature([Message, User, Channel])],
+  controllers: [MessageController],
+  providers: [MessageService],
 })
 export class MessageModule {}
