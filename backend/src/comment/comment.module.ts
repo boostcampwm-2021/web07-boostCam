0fda7940bc152b81df9abbb0e3eb8e36501349a8
K <kihyuk.sung@outlook.kr>
2021-11-24 20:16:21 +0900

    Feat : CommentModule
    comment 관련 controller, service, repository를 사용하기 위한 모듈

--- backend/src/comment/comment.module.ts
+++ backend/src/comment/comment.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './comment.entity';
+import { CommentRepository } from './comment.repository';
+import { CommentService } from './comment.service';
+import { CommentController } from './comment.controller';
+import { MessageRepository } from '../message/message.repository';
+import { User } from '../user/user.entity';
+import { UserServerModule } from '../user-server/user-server.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
-  providers: [],
-  controllers: [],
+  imports: [
+    TypeOrmModule.forFeature([
+      Comment,
+      CommentRepository,
+      MessageRepository,
+      User,
+    ]),
+    UserServerModule,
+  ],
+  providers: [CommentService],
+  controllers: [CommentController],
+  exports: [CommentService],
 })
 export class CommentModule {}
