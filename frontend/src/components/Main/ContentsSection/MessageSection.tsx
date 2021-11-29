6ade8b2ddca3688f3a61b8d6022ee6208d40c1bd
pjs7160@gmail.com <49611158+Suppplier@users.noreply.github.com>
2021-11-29 22:55:23 +0900

    Fix : 불필요한 참조 제거

--- frontend/src/components/Main/ContentsSection/MessageSection.tsx
+++ frontend/src/components/Main/ContentsSection/MessageSection.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useRef } from 'react';
 import styled from 'styled-components';
 
 import { MainStoreContext } from '../MainStore';
