efd77441bc4a415908d902b1cca2182b3e9df77b
K <kihyuk.sung@outlook.kr>
2021-11-30 18:16:28 +0900

    Feat : LoginController에 swagger 적용

--- backend/src/login/login.controller.ts
+++ backend/src/login/login.controller.ts
@@ -1,20 +1,24 @@
 import { Controller, Get, Query, Session } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { ExpressSession } from '../types/session';
+import { UserDto } from '../user/user.dto';
 
 import { LoginService } from './login.service';
 
 @Controller('/api/login')
 export class LoginController {
   constructor(private loginService: LoginService) {}
+
+  @ApiOkResponse({ type: UserDto })
   @Get('/github')
   async githubLogin(
     @Session()
     session: ExpressSession,
     @Query('code') code: string,
-  ) {
+  ): Promise<UserDto> {
     const user = await this.loginService.githubLogin(code);
     session.user = user;
     session.save();
-    return user;
+    return UserDto.fromEntity(user);
   }
 }
