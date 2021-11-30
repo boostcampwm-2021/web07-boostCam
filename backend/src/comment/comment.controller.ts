5969ab7f645f28dfaa02b424e920a0b775e975e2
K <kihyuk.sung@outlook.kr>
2021-11-30 17:59:15 +0900

    Feat : Comment 추가 api 삭제
    Comment 다는 것 또한 http api가 아니라 소켓으로 전송하므로 삭제하였습니다.

--- backend/src/comment/comment.controller.ts
+++ backend/src/comment/comment.controller.ts
@@ -1,29 +1,10 @@
-import { Body, Controller, Get, Post, Query, Session } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import ResponseEntity from '../common/response-entity';
-import { ExpressSession } from '../types/session';
-import { CommentDto } from './comment.dto';
 import { CommentService } from './comment.service';
 
 @Controller('/api/comments')
 export class CommentController {
   constructor(private commentService: CommentService) {}
-
-  @Post()
-  async sendComment(
-    @Session() session: ExpressSession,
-    @Body('channelId') channelId: number,
-    @Body('messageId') messageId: number,
-    @Body('contents') contents: string,
-  ): Promise<ResponseEntity<CommentDto>> {
-    const sender = session.user;
-    const newComment = await this.commentService.sendComment(
-      sender.id,
-      channelId,
-      messageId,
-      contents,
-    );
-    return ResponseEntity.ok(newComment);
-  }
 
   @Get()
   async findCommentsByMessageId(@Query('messageId') messageId: number) {
