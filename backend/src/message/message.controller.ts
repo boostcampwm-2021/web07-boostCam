93dcde41bb020de6bcd47997134cbd7917141c39
K <kihyuk.sung@outlook.kr>
2021-11-30 17:56:39 +0900

    Feat : Message controller swagger 사용
    - ResponseEntity에 ApiProperties 추가
    - UserDto와 MessageDto에 ApiProperties 추가
    - controller에 swagger 적용

--- backend/src/message/message.controller.ts
+++ backend/src/message/message.controller.ts
@@ -1,19 +1,25 @@
 import { Controller, Get, Query, Session, UseGuards } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse } from '@nestjs/swagger';
 import ResponseEntity from '../common/response-entity';
 import { LoginGuard } from '../login/login.guard';
 import { ExpressSession } from '../types/session';
+import { MessageDto } from './message.dto';
+import { MessageDtoSchema } from './message.scheme';
 import { MessageService } from './message.service';
 
 @Controller('/api/messages')
 @UseGuards(LoginGuard)
+@ApiExtraModels(ResponseEntity)
+@ApiExtraModels(MessageDto)
 export class MessageController {
   constructor(private messageService: MessageService) {}
 
+  @ApiOkResponse(MessageDtoSchema)
   @Get()
   async findMessagesByChannelId(
     @Session() session: ExpressSession,
     @Query('channelId') channelId: number,
-  ) {
+  ): Promise<ResponseEntity<MessageDto[]>> {
     const sender = session.user;
     const channelMessages = await this.messageService.findMessagesByChannelId(
       sender.id,
