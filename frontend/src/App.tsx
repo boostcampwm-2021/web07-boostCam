fe7a5084c1feb731e04fa98dac80e2dc98e46338
korung3195 <vs970517@naver.com>
2021-11-30 19:02:54 +0900

    Refactor : LoginPage 디렉토리명 변경
    LoginPage 보다는 Login 이라는 의미가 더 직관적이어서 변경했습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import LoginMain from './components/LoginPage/LoginMain';
+import LoginMain from './components/Login/LoginMain';
 import CamMain from './components/Cam/CamMain';
 import BoostCamMain from './components/Main/BoostCamMain';
-import LoginCallback from './components/LoginPage/LoginCallback';
+import LoginCallback from './components/Login/LoginCallback';
 
 function App(): JSX.Element {
   return (
