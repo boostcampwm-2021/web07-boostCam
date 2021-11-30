d89f7f3eb3b67c544d936384b0fb1979941541f0
K <kihyuk.sung@outlook.kr>
2021-11-30 17:46:29 +0900

    Feat : MessageController에서 사용하지 않는 api 삭제
    - 메시지 전송은 이제 소켓으로 하므로, MessageController에 endpoint는 따로
    필요 없습니다.
    - GET /api/messages/getone 또한 사용하지 않으므로 삭제합니다.

--- backend/src/message/message.controller.ts
+++ backend/src/message/message.controller.ts
@@ -1,53 +1,13 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Query,
-  Session,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Query, Session, UseGuards } from '@nestjs/common';
 import ResponseEntity from '../common/response-entity';
 import { LoginGuard } from '../login/login.guard';
 import { ExpressSession } from '../types/session';
-import { MessageDto } from './message.dto';
 import { MessageService } from './message.service';
 
 @Controller('/api/messages')
 @UseGuards(LoginGuard)
 export class MessageController {
   constructor(private messageService: MessageService) {}
-
-  @Post()
-  async sendMessage(
-    @Session() session: ExpressSession,
-    @Body('channelId') channelId: number,
-    @Body('contents') contents: string,
-  ): Promise<ResponseEntity<MessageDto>> {
-    const sender = session.user;
-    const newMessage = await this.messageService.sendMessage(
-      sender.id,
-      channelId,
-      contents,
-    );
-
-    return ResponseEntity.ok(newMessage);
-  }
-
-  @Get('/getone')
-  async findMessageByMessageId(
-    @Session() session: ExpressSession,
-    @Query('messageId') messageId: number,
-    @Query('channelId') channelId: number,
-  ) {
-    const sender = session.user;
-    const selectedMessage = await this.messageService.findMessagesByMessageId(
-      sender.id,
-      channelId,
-      messageId,
-    );
-    return ResponseEntity.ok(selectedMessage);
-  }
 
   @Get()
   async findMessagesByChannelId(
