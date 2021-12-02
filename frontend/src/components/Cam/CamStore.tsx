8a642f0726dd2d6d4ef4fd3f9619cfc7e3735362
korung3195 <vs970517@naver.com>
2021-12-02 15:58:23 +0900

    Fix : socket 버그 수정
    socket이 이런 저런 말썽을 일으켜서 수정했습니다.

--- frontend/src/components/Cam/CamStore.tsx
+++ frontend/src/components/Cam/CamStore.tsx
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { io } from 'socket.io-client';
+import React, { createContext, useEffect, useRef } from 'react';
+import { io, Socket } from 'socket.io-client';
 
 import useUserMedia from '../../hooks/useUserMedia';
 import { UserInfo } from '../../types/cam';
@@ -10,26 +10,44 @@ type CamStoreProps = {
   setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>;
 };
 
-const socket = io({
-  withCredentials: true,
-  forceNew: true,
-});
-
 export const CamStoreContext = createContext<React.ComponentState>(null);
 
 function CamStore(props: CamStoreProps): JSX.Element {
   const { children, userInfo, setUserInfo } = props;
   const currentURL = new URL(window.location.href);
   const roomId = currentURL.searchParams.get('roomid');
+
+  const socketRef = useRef<Socket>();
+
+  if (!socketRef.current) {
+    const socket = io('/cam', {
+      withCredentials: true,
+      forceNew: true,
+      transports: ['polling'],
+    });
+
+    socketRef.current = socket;
+  }
+
   const { localStatus, localStream, setLocalStatus, screenList } = useUserMedia({
-    socket,
+    socket: socketRef.current,
     roomId,
     userInfo,
   });
 
+  useEffect(() => {
+    const socketDisconnect = () => {
+      socketRef?.current?.disconnect();
+    };
+    window.addEventListener('popstate', socketDisconnect);
+    return () => {
+      window.removeEventListener('popstate', socketDisconnect);
+    };
+  }, []);
+
   return (
     <CamStoreContext.Provider
-      value={{ socket, localStream, localStatus, setLocalStatus, screenList, userInfo, setUserInfo }}
+      value={{ socket: socketRef.current, localStream, localStatus, setLocalStatus, screenList, userInfo, setUserInfo }}
     >
       {children}
     </CamStoreContext.Provider>
